104832129dddd5fdefe464f17ba780953e013db9
Linh B Ngo <lngo@wcupa.edu>
2020-02-26 16:20:52 -0500

    Update install_kubernetes.sh

--- install_kubernetes.sh
+++ install_kubernetes.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# ensure legacy binaries are installed
-sudo apt-get install -y iptables arptables ebtables
-
-# switch to legacy versions
-sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
-sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-sudo update-alternatives --set arptables /usr/sbin/arptables-legacy
-sudo update-alternatives --set ebtables /usr/sbin/ebtables-legacy
-
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list deb https://apt.kubernetes.io/ kubernetes-xenial main EOF
-sudo apt-get update
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
