9e1341b457d9064bee7e0c51f7fab1ac25f9ead7
Linh B Ngo <lngo@wcupa.edu>
2020-02-26 14:02:26 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -20,7 +20,8 @@ request.addTour(tour)
 prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
-for i in range(3):
+num_nodes = 3
+for i in range(num_nodes):
   if i == 0:
     node = request.XenVM("head")
   else:
@@ -33,6 +34,24 @@ for i in range(3):
   iface.component_id = "eth1"
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
+  
+  # setup NFS
+  if i == 0:
+    node.addService(RSpec.Execute("sh", "sudo apt-get update"))
+    node.addService(RSpec.Execute("sh", "sudo apt-get install -y nfs-kernel-server"))
+    node.addService(RSpec.Execute("sh", "sudo mkdir -p /opt/keys"))
+    node.addService(RSpec.Execute("sh", "sudo chown nobody:nogroup /opt/keys"))
+    for k in range(1,num_nodes):
+      node.addService(RSpec.Execute("sh", "sudo echo '/opt/keys 192.168.1." + str(k+1) + "(rw,sync,no_root_squash,no_subtree_check)' | sudo tee -a /etc/exports"))
+    node.addService(RSpec.Execute("sh", "sudo systemctl restart nfs-kernel-server"))
+  else:
+    node.addService(RSpec.Execute("sh", "sudo apt-get update"))
+    node.addService(RSpec.Execute("sh", "sudo apt-get install -y nfs-common"))
+    node.addService(RSpec.Execute("sh", "sudo mkdir -p /opt/keys"))
+    node.addService(RSpec.Execute("sh", "sleep 3m"))
+    node.addService(RSpec.Execute("sh", "sudo mount 192.168.1.1:/opt/keys /opt/keys"))
+  
+  # setup Docker
   node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
   
 pc.printRequestRSpec(request)
