6355116151d6b2fed3fa377749a62a2d3cee44f0
Linh B Ngo <lngo@wcupa.edu>
2020-04-07 15:01:08 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -28,7 +28,7 @@ for i in range(3):
   node.cores = 4
   node.ram = 8192
   node.routable_control_ip = "true" 
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-ARM"
   iface = node.addInterface("if" + str(i))
   iface.component_id = "eth1"
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
