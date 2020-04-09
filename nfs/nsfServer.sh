bafef1a9303f3ea2ffe50363cc55750f66b160a5
Linh B Ngo <lngo@clemson.edu>
2020-04-09 14:18:12 -0400

    Update nsfServer.sh

--- nfs/nsfServer.sh
+++ nfs/nsfServer.sh
@@ -14,27 +14,7 @@ sudo chmod -R a+rx /opt/keys
 sudo apt-get install -y nfs-kernel-server
 
 # Create the permissions file for the NFS directory.
-computes=$(($1 + 1))
-for i in $(seq 2 $computes)
-do
-  echo "/home 192.168.1.$i(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-  echo "/mpishare 192.168.1.$i(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-  echo "/opt 192.168.1.$i(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-  echo "/software 192.168.1.$i(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-done
-
-# Let the Slurm node (head) have access to the shared directories.
-slurmip=$(($2 + 1))
-echo "/home 192.168.1.$slurmip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-echo "/mpishare 192.168.1.$slurmip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-echo "/opt 192.168.1.$slurmip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-echo "/software 192.168.1.$slurmip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-
-## Let the login node have access to the shared directories.
-#loginip=$(($3 + 1))
-#echo "/home 192.168.1.$loginip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-#echo "/mpishare 192.168.1.$loginip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-#echo "/opt 192.168.1.$loginip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-#echo "/software 192.168.1.$loginip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+echo "/opt/keys 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+echo "/opt/keys 192.168.1.3(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 
 sudo systemctl restart nfs-kernel-server
