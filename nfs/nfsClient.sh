dacc8a628e60114f63b0c92c324ba8586ffc6fa5
Linh B Ngo <lngo@clemson.edu>
2020-04-09 14:13:57 -0400

    Create nfsClient.sh

---
+++ nfs/nfsClient.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "installNfsClient.sh"
+
+USERNAMELIST=$(getent passwd {1000..60000} | sed 's/:.*//')
+
+set -x
+
+sudo apt-get update
+
+sudo apt-get install -y nfs-common
+
+sudo mkdir /mpishare
+sudo mkdir /opt/shared
+sudo mkdir /software
+
+sudo mount 192.168.1.1:/software /software || true
+# Cycle until we can mount software.
+while [ ! -d /software/flagdir ]; do
+  sudo mount 192.168.1.1:/software /software || true
+  sleep 60
+done
+sudo mount 192.168.1.1:/home /home
+sudo mount 192.168.1.1:/mpishare /mpishare
+sudo mount 192.168.1.1:/opt /opt/shared
+
+# Keep the shared dirs after a reboot
+echo '192.168.1.1:/home /home nfs' | sudo tee -a /etc/fstab
+echo '192.168.1.1:/mpishare /mpishare nfs' | sudo tee -a /etc/fstab
+echo '192.168.1.1:/opt /opt/shared nfs' | sudo tee -a /etc/fstab
+echo '192.168.1.1:/software /software nfs' | sudo tee -a /etc/fstab
+
+# Change user home dirs
+for i in $USERNAMELIST 
+do
+    sudo usermod -d /home/$i $i
+done
