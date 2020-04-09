569bb8f57230e717e876130ae5375ffba8f5c14e
Linh B Ngo <lngo@clemson.edu>
2020-04-09 14:21:12 -0400

    Update nsfServer.sh

--- nfs/nsfServer.sh
+++ nfs/nsfServer.sh
@@ -18,3 +18,4 @@ echo "/opt/keys 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee
 echo "/opt/keys 192.168.1.3(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 
 sudo systemctl restart nfs-kernel-server
+mkdir -p /opt/keys/flagdir
