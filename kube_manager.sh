c7dfe0f9d9ccddec952280728336ebe3d0dc1870
Linh B Ngo <lngo@clemson.edu>
2020-04-20 16:47:44 -0400

    Update kube_manager.sh

--- kube_manager.sh
+++ kube_manager.sh
@@ -7,3 +7,5 @@ chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
+
+kubectl apply -f /local/repository/dashboard-adminuser.yaml
