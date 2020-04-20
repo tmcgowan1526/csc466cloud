b292f1c20909d602ab3f0372e68a4d7fccb7b68c
Linh B Ngo <lngo@clemson.edu>
2020-04-20 16:46:29 -0400

    Update kube_manager.sh

--- kube_manager.sh
+++ kube_manager.sh
@@ -5,3 +5,5 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
