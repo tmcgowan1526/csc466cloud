7ba17ebb122ae29c17566a33140d2688e43660d0
Linh B Ngo <lngo@clemson.edu>
2020-04-20 16:17:40 -0400

    Create kube_manager.sh

---
+++ kube_manager.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+kubeadm init > /opt/keys/kube.log
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
