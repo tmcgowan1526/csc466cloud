3197eaf7163bdff687113953e1103e1598534261
Linh B Ngo <lngo@clemson.edu>
2020-04-21 14:48:44 -0400

    Update kube_manager.sh

--- kube_manager.sh
+++ kube_manager.sh
@@ -13,9 +13,3 @@ sudo systemctl restart nfs-kernel-server
 
 kubeadm init > /opt/keys/kube.log
 
-#mkdir -p /root/.kube
-#cp -i /etc/kubernetes/admin.conf /root/.kube/config
-#chown root:root /root/.kube/config
-#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
-#kubectl apply -f /local/repository/dashboard-adminuser.yaml
