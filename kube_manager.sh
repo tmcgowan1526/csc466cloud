20f2fbc910c2f853acf0b14da2d737511ea7db68
Linh B Ngo <lngo@clemson.edu>
2020-04-21 13:11:39 -0400

    Update kube_manager.sh

--- kube_manager.sh
+++ kube_manager.sh
@@ -10,6 +10,8 @@ echo "/opt/keys 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee
 echo "/opt/keys 192.168.1.3(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 
 sudo systemctl restart nfs-kernel-server
+sudo mkdir /opt/keys/flagdir
+
 kubeadm init > /opt/keys/kube.log
 mkdir -p /root/.kube
 cp -i /etc/kubernetes/admin.conf /root/.kube/config
